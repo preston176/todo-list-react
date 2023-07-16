cef1e146b59eeb564b4c3258929a1671c5bb9934
preston176 <prestonnyamweya@gmail.com>
2023-07-17 01:25:46 +0300

    short circuiting + everything working as 1

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,11 @@ function toggleTodo (id, completed) {
   })})
 }
 
+function deleteTodo (id) {
+  setTodos(currentTodos => {
+    return currentTodos.filter(todo => todo.id !== id)
+  })}
+
   return (
     <div className='wrapper bg-slate-600 h-auto w-3/4 rounded-lg m-auto max-h-fit'>
       
@@ -36,6 +41,7 @@ function toggleTodo (id, completed) {
     </form>
     <h1 className='header text-neutral-100 font-light text-3xl'>Todo List</h1>
     <ul className='list text-white m-3.5 p-3.5'>
+    {todos.length === 0 && <p className='text-center text-2xl'>No Todos</p>}
     {todos.map(todo => { 
       return (
       
@@ -44,7 +50,7 @@ function toggleTodo (id, completed) {
           <input type="checkbox" className='mx-3.5' checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)}/>
           {todo.title}
         </label>
-        <Button variant="outlined" color="error" size='medium'>Delete</Button>
+        <Button onClick={() => deleteTodo(todo.id)} variant="outlined" color="error" size='medium'>Delete</Button>
         {/* <button className='btn btn-danger '>Delete</button> */}
       </li>)
     })}
