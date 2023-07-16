5a70b625377b4daa4b311ae59ee214a4ec1eae91
preston176 <prestonnyamweya@gmail.com>
2023-07-17 00:22:13 +0300

    tailwind styling + gradient background

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,32 @@ export default function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='wrapper bg-slate-600 h-96 w-auto rounded-lg m-auto '>
       
-    <form className='new-item-form'>
-      <div className='form-row'>
-        <label htmlFor='item'>New Item</label>
-        <input type="text" id='item' />
+    <form className=' new-item-form py-3.5 flex-col flex px-2.5'>
+      <div className='form-row flex flex-col'>
+        <label className='text-neutral-100 text-center text-3xl font-normal h-10  ' htmlFor='item'>New Item</label>
+        <input type="text" id='item' className='rounded-md outline-none border-none hover:outline-none hover:border-none active:border-none active:outline-none h-7 transition-all duration-300'/>
       </div>
-      <button className='btn'>Add</button>
+      <button className='btn bg-sky-700 hover:bg-sky-500 text-white my-2 m-auto w-full h-8 rounded-lg'>Add</button>
     </form>
-    <h1 className='header'>Todo List</h1>
-    <ul className='list'>
-      <li>
-        <label htmlFor="">
-          <input type="checkbox" />
+    <h1 className='header text-neutral-100 font-light text-3xl'>Todo List</h1>
+    <ul className='list text-white m-3.5 p-3.5'>
+      <li className='my-1'>
+        <label htmlFor="" >
+          <input type="checkbox"  />
           Item 1
         </label>
-        <button className='btn btn-danger'>Delete</button>
+        <button className='btn btn-danger mx-3.5'>Delete</button>
       </li>
       <li>
         <label htmlFor="">
           <input type="checkbox" />
           Item 2
         </label>
-        <button className='btn btn-danger'>Delete</button>
+        <button className='btn btn-danger mx-3.5'>Delete</button>
       </li>
     </ul>
-    </>
+    </div>
   )
 }
