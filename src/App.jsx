cac75e8052032ec3e28b17ce1d970b24141770c9
preston176 <prestonnyamweya@gmail.com>
2023-07-17 02:22:24 +0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import NewTodoForm from './components/NewTodoForm';
 import TodoList from './components/TodoList';
+import StickyFooter from './components/MUI/StickyFooter';
 
 export default function App() {
   
@@ -11,7 +12,7 @@ browser's `localStorage` with the key `'ITEMS'`. */
     const localValue = localStorage.getItem('ITEMS')
     if (localValue == null) return []
 
-    
+
     return JSON.parse(localValue)
   });
 /* The `useEffect` hook is used to perform side effects in a functional component. In this case, the
@@ -44,10 +45,14 @@ function deleteTodo (id) {
   })}
 
   return (
+    <>
     <div className='wrapper bg-slate-600 h-auto w-3/4 rounded-lg m-auto max-h-fit'>
       <NewTodoForm onSubmit={addTodo}/>
     <h1 className='header text-neutral-100 font-light text-3xl'>Todo List</h1>
     <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+    
     </div>
+    <StickyFooter />
+    </>
   )
 }
