e08a0f46fd9467d79f290a18e0b4ee2a5fc893c7
preston176 <prestonnyamweya@gmail.com>
2023-07-17 00:51:26 +0300

    input styles and mui introduced

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import './App.css'
+import { Button }  from '@mui/material';
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [newItem, setNewItem] = useState("");
 
   return (
     <div className='wrapper bg-slate-600 h-96 w-auto rounded-lg m-auto '>
@@ -10,9 +11,9 @@ export default function App() {
     <form className=' new-item-form py-3.5 flex-col flex px-2.5'>
       <div className='form-row flex flex-col'>
         <label className='text-neutral-100 text-center text-3xl font-normal h-10  ' htmlFor='item'>New Item</label>
-        <input type="text" id='item' className='rounded-md outline-none border-none hover:outline-none hover:border-none active:border-none active:outline-none h-7 transition-all duration-300'/>
+        <input type="text" value={newItem} onChange={e => {setNewItem(e.target.value)}} id='item' className='rounded-md outline-none border-none hover:outline-none hover:border-none active:border-none active:outline-none h-7 bg-emerald-700 text-white font-bold text-center'/>
       </div>
-      <button className='btn bg-sky-700 hover:bg-sky-500 text-white my-2 m-auto w-full h-8 rounded-lg'>Add</button>
+      <button className='btn bg-sky-700 hover:bg-sky-500 text-white my-2 m-auto w-full h-8 rounded-lg border-none outline-none'>Add</button>
     </form>
     <h1 className='header text-neutral-100 font-light text-3xl'>Todo List</h1>
     <ul className='list text-white m-3.5 p-3.5'>
@@ -21,7 +22,8 @@ export default function App() {
           <input type="checkbox"  />
           Item 1
         </label>
-        <button className='btn btn-danger mx-3.5'>Delete</button>
+        <Button variant="outlined" color="error" size='medium'>Delete</Button>
+        {/* <button className='btn btn-danger '>Delete</button> */}
       </li>
       <li>
         <label htmlFor="">
