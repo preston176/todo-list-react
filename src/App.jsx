d4a47aee862a0b604009033d824ea82b59877595
preston176 <prestonnyamweya@gmail.com>
2023-07-25 22:04:25 +0300

    Background color

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import './App.css'
 
 function App() {
 
+  const style={
+    bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`
+  }
+
   return (
-    <>
-      <h2>todo list in firebase</h2>
-    </>
+    <div className={style.bg}>
+    
+    </div>
   )
 }
 
